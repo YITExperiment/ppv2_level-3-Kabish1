a22870bbcfa992dd961d24d6573b5cc0729e05a5
Kabish1 <135015592+Kabish1@users.noreply.github.com>
2023-06-23 20:41:24 +0530

    Add files via upload

---
+++ my firstprogram.py
@@ -0,0 +1,19 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========================================== RESTART: C:/Users/User/AppData/Local/Programs/Python/Python311/1007Kabish.py ==========================================
+Hello, World!
+>>> 
+=========================================== RESTART: C:/Users/User/AppData/Local/Programs/Python/Python311/1007Kabish.py ==========================================
+Hello, World!
+What is your name?Kabish
+Hello Kabish
+>>> age=12
+>>> print(age)
+12
+>>> Weight=29
+>>> print(Weight)
+29
+>>> Height=145
+>>> print(Height)
+145
